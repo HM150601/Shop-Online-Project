99d82fe760edf7d7e06534d2877659646a4d4a14
Nguyễn Lê Hoàng Minh <minh150601@gmail.com>
2022-12-10 22:38:18 +0700

    Revert "Merge pull request #1 from HM150601/main"
    This reverts commit db9c7228716a5b5fd41eea1fccc3d6b0f7de6d9a, reversing
    changes made to 581e7334a0c9800150366fe5bd5bd319623dba00.

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -7,7 +7,6 @@ import { login } from "../redux/actions/userActions";
 import Loading from "../components/Loading";
 import Message from "../components/Message";
 import Meta from "../components/Meta";
-import { signInWithGoogle } from "../firebase"
 
 const LoginScreen = () => {
   let navigate = useNavigate();
@@ -31,15 +30,6 @@ const LoginScreen = () => {
     e.preventDefault();
     if (email !== "" && password !== "") dispatch(login(email, password));
   };
-
-  const handelLoginWithGoogle = async () => {
-    try{
-      await signInWithGoogle();
-      setEmail(localStorage.getItem("googleEmail"));
-    } catch{
-
-    }
-  }
 
   return (
     <>
@@ -66,23 +56,6 @@ const LoginScreen = () => {
               onChange={(e) => setPassword(e.target.value)}
             ></Form.Control>
           </Form.Group>
-          <Button
-          onClick={handelLoginWithGoogle}
-          className="d-flex gap-2 justify-content-center align-items-center mt-3 w-100 btn-danger"
-          type="button"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="18"
-            height="18"
-            fill="currentColor"
-            className="bi bi-google"
-            viewBox="0 0 16 16"
-          >
-            <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
-          </svg>
-          Login With Google
-        </Button>
           <Button className="mt-3" type="submit" variant="primary">
             {loading ? <Loading /> : `Sign In`}
           </Button>
@@ -90,6 +63,11 @@ const LoginScreen = () => {
         <Row className="py-3">
           <Col>
             New Customer? <Link to="/register">Register</Link>
+          </Col>
+        </Row>
+        <Row className="py-2">
+          <Col>
+             <Link to="/forget">Forget Password</Link>
           </Col>
         </Row>
       </FormContainer>
